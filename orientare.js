292cb40ec5675d6cceba816e1deda30a770b83e1
Daniela Onita <danielaonita25@gmail.com>
2020-10-15 16:55:48 +0300

    Update orientare.js

--- orientare.js
+++ orientare.js
@@ -1,4 +1,5 @@
 window.addEventListener("deviceorientation", on_device_orientation);
+
 function on_device_orientation(evt)
 {
 	var alpha = evt.alpha;
@@ -15,10 +16,14 @@ function on_device_orientation(evt)
 	var latura_patrat = 50;
 	
 	context.setTransform(1, 0, 0, 1, 0, 0);
+	
 	context.clearRect(0,0,canvas.width, canvas.height);
+	
 	context.translate(centru.x/2, centru.y/2);
+	
 	context.rotate(alpha * Math.PI / 180);
+	
 	context.beginPath();
-	context.rect(-latura_patrat/2,-latura_patrat/2, latura_patrat, latura_patrat);
+	context.rect(-latura_patrat/2, -latura_patrat/2, latura_patrat, latura_patrat)
 	context.stroke();
 }
